144867e3f5f45c1fc15eeed2ba7f476bfd593747
Gabin <35979202+wewlr17@users.noreply.github.com>
2019-01-26 17:14:52 +0100

    UPD lot of md

--- convertMdToHtml/convert_md.py
+++ convertMdToHtml/convert_md.py
@@ -3,6 +3,8 @@
 import markdown
 import codecs
 import argparse
+from pathlib import Path
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", type=str,help="dossier markdown")
@@ -11,6 +13,7 @@ args = parser.parse_args()
 
 if args.input != None:
     pathMd = args.input
+    pathMd = pathMd + '/'
 else:
     pathMd = "./Markdown/"
     pass
@@ -20,12 +23,14 @@ else:
     pathSite = "./sitestatique"
     pass
 
-def convert_to_html(pathMd):
+def convert_to_html(pathMd, p, index):
     check = 0
-    if pathMd != None and pathMd != "./Markdown/markdown.md":
-        md = pathMd + "markdown.md"
+    if pathMd != None and pathMd != "./Markdown/markdown1.md":
+        md = pathMd + "markdown1.md"
     else:
         pass
+    if index > 1:
+        md = "./Markdown/" + p[index-1].name
     fichier_md = codecs.open(md, mode="r", encoding="utf-8")
     text = fichier_md.read()
     html = markdown.markdown(text)
@@ -41,11 +46,14 @@ def convert_to_html(pathMd):
         fichier_set.write(ligne.replace('\n', '\n\t'))
     fichier.close()
 
-def create_html(pathSite):
+def create_html(pathSite, index):
+    nbr = str(index)
     if pathSite != None and pathSite != "./sitestatique/HTML/index.html":
         site = pathSite + "/HTML/index.html"
     else:
         pass
+    if index > 1:
+        site = "./sitestatique/HTML/page" + nbr + ".html"
     fichier = open(site, "w")
     fichier = open(site, "a")
     base_html = codecs.open("./template/basehtml.txt", mode="r", encoding="utf-8")
@@ -65,7 +73,14 @@ def create_css():
     txt_baseCss = base_css.read()
     fichier.write(txt_baseCss)
 
+def loop_convert(pathMd, pathSite, p, nbr_md):
+    index = 1
+    while index != nbr_md+1:
+        convert_to_html(pathMd, p, index)
+        create_html(pathSite, index)
+        index = index + 1
+    create_css()
 
-convert_to_html(pathMd)
-create_html(pathSite)
-create_css()
+p = sorted(Path(pathMd).glob('*.md'))
+nbr_md = len(p)
+loop_convert(pathMd, pathSite, p, nbr_md)
